4097ffa0c5c7efe047842006c98c74ab4d4b42bc
Anar <opofiz1992@gmail.com>
2023-02-11 19:36:52 +0700

    задача с днями недели

--- seminar/Program.cs
+++ seminar/Program.cs
@@ -10,17 +10,14 @@
 -3 -> 9
 -7 -> 49*/
 
-Console.Clear();
-Console.Write("Введите число 1: ");
-int numberA = int.Parse(Console.ReadLine());
-Console.Write("Введите число квадрат первого: ");
-int numberB = int.Parse(Console.ReadLine());
-int sqr = numberA * numberA;
-    if (numberB == sqr)
-        {
-        Console.WriteLine("Правильно! ");
-        }
-        else
-        {
-    Console.WriteLine("Не правильно! ");
-        }
+Console.Write("Введите число от 1 до 7");
+int a = int.Parse(Console.ReadLine());
+if (a < 0)Console.WriteLine("Введите число от 1 до 7");
+if (a > 7)Console.WriteLine("Введите число от 1 до 7");
+if (a == 1)Console.WriteLine("Понедельник");
+if (a == 2)Console.WriteLine("Вторник");
+if (a == 3)Console.WriteLine("Среда");
+if (a == 4)Console.WriteLine("Четверг");
+if (a == 5)Console.WriteLine("Пятница");
+if (a == 6)Console.WriteLine("Суббота");
+if (a == 7)Console.WriteLine("Воскресенье");
