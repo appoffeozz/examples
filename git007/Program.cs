876d7cae942f9b59443114477a889594bb5066c3
Anar <opofiz1992@gmail.com>
2023-02-14 20:23:06 +0700

    задача 1

---
+++ git007/Program.cs
@@ -0,0 +1,8 @@
+﻿int a, b;
+        Console.Write("Введите первое число: ");
+        a = int.Parse(Console.ReadLine());
+        Console.Write("Введите второе число: ");
+        b = int.Parse(Console.ReadLine());
+        if (a > b)
+            Console.WriteLine("{0} - максимальное значение, {1} - минимальное значение", a, b);
+        else Console.WriteLine("{0} - максимальное значение, {1} - минимальное значение", b, a);
